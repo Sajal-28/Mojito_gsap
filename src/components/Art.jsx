32d93d8cf73bc7e931e9125131f33d3b3dca7e75
Sajal Dubey <dubeysajal007@gmail.com>
2026-04-02 16:57:28 +0530

    Fixed Bugs of the Art Section

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -70,7 +70,7 @@ const Art = () => {
                 </ul>
             </div>
 
-            <div className='masked-content'>
+            <div className='masked-container'>
                 <h2 className='will-fade'>Sip-Worthy Perfection</h2>
                 <div id='masked-content'>
                     <h3>Made with Craft, Poured with Passion</h3>
